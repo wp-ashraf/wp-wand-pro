31934416e3730d5efa0cdfcc48c167767074d803
Ashraf Uddin <ashrafuddin765@gmail.com>
2025-10-26 09:30:18 +0600

    Release V1.3.01

--- wp-wand-pro.php
+++ wp-wand-pro.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Wand Pro
  * Plugin URI: https://wpwand.com/
  * Description: WP Wand Pro allows you to use the full potential of WP Wand with tons of extra features for quality content generation.
- * Version: 1.3.0
+ * Version: 1.3.01
  * Author: WP Wand
  * Author URI: https://wpwand.com/
  * Text Domain: wp-wand-pro
@@ -24,12 +24,12 @@ define('WPWAND_PRO_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 
 
-
+require __DIR__ . '/vendor/action-scheduler/action-scheduler.php';
+require __DIR__ . '/vendor/action-scheduler/action-scheduler-high-volume.php';
 
 function wpwand_pro_loaded()
 {
-    require __DIR__ . '/vendor/action-scheduler/action-scheduler.php';
-    require __DIR__ . '/vendor/action-scheduler/action-scheduler-high-volume.php';
+
 
     add_action('init', 'wpwand_pro_load_plugin', 15);
 }
